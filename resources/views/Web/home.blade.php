52b8fea03c1f48cb60b15e3af78cfd10f0913426
Fellipe <fellipeanthony1@hotmail.com>
2022-11-02 23:14:22 -0300

    Add Tailwind css

--- resources/views/Web/home.blade.php
+++ resources/views/Web/home.blade.php
@@ -3,5 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
-
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 @endsection
--- resources/views/Web/layouts/master.blade.php
+++ resources/views/Web/layouts/master.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <title> @yield('title') </title>
+    <!-- <link rel="icon" href=''> -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title> @yield('title') </title>
+    <link rel="stylesheet" href="{{ asset('assets/web/master.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
     <body class='body-content'>
         <header class='header-content'>

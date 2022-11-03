c2e2d5986f5e396d2b278e8f16a81ab49de4b10d
Fellipe <fellipeanthony1@hotmail.com>
2022-11-02 21:36:55 -0300

    Core routes, apply middlewares guest and auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('home')->group(function() {
+    Route::controller(HomeController::class)->group(function () {
+        Route::get('/terms', 'terms')->name('home.terms');
+        Route::get('/highscores', 'highscores')->name('home.highscores');
+        Route::get('/download', 'download')->name('home.download');
+        Route::get('/guilds', 'guilds')->name('home.guilds');
+
+        Route::middleware(['guest'])->group(function() {
+            Route::get('/register', 'register')->name('home.register');
+            Route::get('/login', 'login')->name('home.login');
+        });
+    });
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('user')->group(function() {
+        Route::controller(UserController::class)->group(function () {
+            Route::get('/profile', 'profile')->name('user.profile');
+            Route::get('/createcharacter', 'create')->name('user.createcharacter');
+            Route::get('/changepassword', 'changePassword')->name('user.changepassword');
+            Route::get('/logout', 'logout')->name('user.logout');
+        });
+    });
+});
+
+Route::prefix('dashboard')->group(function() {
+
 });
